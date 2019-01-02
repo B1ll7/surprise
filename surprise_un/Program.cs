a2f925ad80c3efbe1cf865ef3c468241acd3e279
B1ll7 <malarbillaudrey@gmail.com>
2019-01-02 11:21:29 +0100

    Add modification in the code

--- surprise_un/Program.cs
+++ surprise_un/Program.cs
@@ -13,25 +13,35 @@ namespace surprise_un
             // je vais essayer d'afficher un sapin de noël avec des slash et des anti-slash ansi que des pipe 
             String usergender;
             String username;
+            String space;
             int usernumber;
             int counter;
 
             Console.WriteLine("Are you a mr or a mrs?");
-            usergender = Console.ReadLine();
+            usergender = Console.ReadLine();// Recupère le genre l'utilisateur
             Console.WriteLine("what's your name " + usergender + "?");
-            username = Console.ReadLine();
+            username = Console.ReadLine(); // Recupère le nom de l'utilisateur
             Console.WriteLine("Enter a number please more than 0");
-            usernumber = Console.ReadLine();
-            Console.WriteLine("Merry Christmast " + usergender + " " + username + " ! \x01");
+            usernumber = int.Parse(Console.ReadLine()); //enregistre un nombre, utile pour plus tard
+            Console.WriteLine("Merry Christmast " + usergender + " " + username + " ! \x01"); // Affiche le joyeux noël à l'utilisateur
+            // A partir de la nous allons essayer de dessiner le sapin
             counter = 0;
-            while (counter < 10)
+            space = " ";
+            while(usernumber <= 0)
+            { Console.WriteLine("We thinks there is a mistake in the number please choose a number more than 0"); } // Elimine le cas 0 et les cas negatifs 
+            if(usernumber > 0)
             {
-                Console.WriteLine("|");
-                i++;
+                Console.SetCursorPosition(10, 1);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("*");
+                while (counter < usernumber)
+                {
+                    Console.WriteLine("/\\");
+                    counter++;
+                }
+                Console.SetCursorPosition(10, 0);
+                Console.WriteLine("||");
             }
-
-
-
         }
     }
 }
